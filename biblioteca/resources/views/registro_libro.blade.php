7432e9893968607cdb89ada4a8e78d82fa145777
mgfuentess <mariaguadalupefuentes9917@gmail.com>
2024-10-31 01:03:54 -0600

    corregimos errores en la plantilla

--- biblioteca/resources/views/registro_libro.blade.php
+++ biblioteca/resources/views/registro_libro.blade.php
@@ -9,28 +9,40 @@
 <body>
     {{-- inicia navbar --}}
     <nav class="navbar bg-body-tertiary fixed-top">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Biblioteca UPQ</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-      <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Más</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-      </div>
-      <div class="offcanvas-body">
-        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Inicio</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Registro del libro</a>
-          </li>
-        </ul>
-      </div>
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Biblioteca UPQ</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+        <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Más</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body">
+            <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Registro del libro</a>
+            </li>
+            </ul>
+        </div>
+        </div>
     </div>
-  </div>
-</nav>
+    </nav>
+    {{-- Finaliza navbar --}}
+
+    <!-- Especificamos donde ira el contenido -->
+     @yield('contenido')
+
+
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">©Biblioteca UPQ</h5>
+            <p class="card-text">31 de Octubre del 2024</p>
+        </div>
+    </div>
 </body>
 </html>
