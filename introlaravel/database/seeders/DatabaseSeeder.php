0dfe608565ca72af10d1e3103cf2d19531153a05
mgfuentess <mariaguadalupefuentes9917@gmail.com>
2024-11-12 20:28:15 -0600

    Practica 7

--- introlaravel/database/seeders/DatabaseSeeder.php
+++ introlaravel/database/seeders/DatabaseSeeder.php
@@ -13,11 +13,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+
+        $this->call(clienteSeeder::class);
+
+
         // User::factory(10)->create();
 
-        User::factory()->create([
+        /* User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
-        ]);
+        ]); */
     }
 }
